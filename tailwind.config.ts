20bac3f400af4b2898cdee949167f812f2eb24a0
Divyansh Patel <pateldivyansh131@gmail.com>
2025-04-17 03:59:45 +0530

    final code

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+// tailwind.config.ts
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './index.html',
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
